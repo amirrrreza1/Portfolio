b2432891bffaf6fa7990baaa6daaae41f3c9c18f
Amirreza <arazarioun83@gmail.com>
2025-08-18 22:11:38 +0330

    Feat: add Custom Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import ThemeToggle from "@/Components/UI/Buttons/ThemeToggle";
-
 const HomePage = () => {
-  return (
-    <main>
-      <div className="min-h-screen flex justify-center items-center bg-primary text-secondary">
-        Hello World
-      </div>
-    </main>
-  );
+  return <main></main>;
 };
 
 export default HomePage;
