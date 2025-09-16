66062a3155b19061bb043b0dc532d3d1a5bd090e
Amirreza <arazarioun83@gmail.com>
2025-09-16 23:04:20 +0330

    Fix: Hero section responsive

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -5,16 +5,16 @@ const Hero = () => {
   return (
     <>
       <main
-        className="h-[calc(100vh-80px)] flex flex-col lg:flex-row justify-center items-center Container"
+        className="h-svh flex flex-col lg:flex-row justify-center items-center gap-5 Container"
         id="home"
       >
         <section className="lg:w-1/2 h-fit">
-          <div className="w-fit flex flex-col justify-center items-start gap-4 p-2 rounded">
+          <div className="w-full flex flex-col justify-center items-start gap-4 p-2 rounded">
             <CodeStyleText
               strings={["Hello There!", "I'm Amirreza Azarioun"]}
               typingSpeed={50}
               deletingSpeed={30}
-              className="text-xl lg:text-3xl"
+              className="text-xl md:text-2xl lg:text-3xl min-w-[270px]"
               pauseBetween={3000}
             />
             <p className="text-sm lg:text-lg lg:text-center">
@@ -22,7 +22,7 @@ const Hero = () => {
             </p>
           </div>
         </section>
-        <section className="w-full lg:w-1/2 h-full flex justify-center items-end">
+        <section className="w-full h-[300px] lg:w-1/2 flex justify-center items-end">
           <RubikCube />
         </section>
       </main>
